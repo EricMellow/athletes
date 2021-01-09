b024601f938d5e87985cac6b9725e0409bf8a77a
Eric Mellow <ericmellow@gmail.com>
2021-01-09 10:23:29 -0700

    Change Home to class component and get Users

--- code/src/components/home/Home.tsx
+++ code/src/components/home/Home.tsx
@@ -1,14 +1,40 @@
+import React, { Component } from 'react';
 import logo from "../../assets/logo.svg";
+import getUsers from "../../api/getUsers"
 
-export default function Home() {
-  return (
-    <div className="home">
-      <div className="body">
-        <img src={logo} className="logo" alt="TrainHeroic Logo" />
-        <h1>
-          My Athletes
-        </h1>
+class Home extends Component {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      athletes: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllAthletes();
+  }
+
+  getAllAthletes = async () => {
+    const athletes = await getUsers();
+    this.setState({
+      athletes
+    });
+    console.log(athletes)
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="body">
+          <img src={logo} className="logo" alt="TrainHeroic Logo" />
+          <h1>
+            My Athletes
+          </h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
+
+export default Home;
