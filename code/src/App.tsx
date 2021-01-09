71ed0561749f439855155133d4f8c0f2def57635
Eric Mellow <ericmellow@gmail.com>
2021-01-09 12:39:02 -0700

    Connect React Router

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.scss";
 import Home from "./components/home/Home";
+import Profile from "./components/profile/Profile";
+import {
+    Switch,
+    Route
+} from "react-router-dom";
 
 export default function App() {
     return (
         <div className="trainheroic-code-test">
-            <Home />
+            <Switch>
+                <Route path="/">
+                    <Home />
+                </Route>
+                <Route path="/athlete/:id">
+                    <Profile />
+                </Route>
+            </Switch>
         </div>
     );
 }
