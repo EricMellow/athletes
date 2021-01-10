3847858a20b900a6b8ac807a8cdb198b6d8c558b
Eric Mellow <ericmellow@gmail.com>
2021-01-10 16:19:56 -0700

    Add HTML for Nav component

--- code/src/components/nav/Nav.tsx
+++ code/src/components/nav/Nav.tsx
@@ -1,17 +1,28 @@
-import "./Nav.scss"
-import {
-  useLocation
-} from "react-router-dom";
-
-interface RouteParams {
-  id: string
-}
+import "./Nav.scss";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Nav() {
   let location = useLocation();
   console.log(location)
 
-  return (
-    <nav></nav>
-  )
+  if (location.pathname !== "/") {
+    return (
+      <nav className="profile-nav">
+        <Link to="/" className="back-button">
+          <h3>Back</h3>
+        </Link>
+        <div className="logo-container">
+          <img src={logo} className="logo" alt="TrainHeroic Logo" />
+        </div>
+      </nav>
+    )
+  } else {
+    return (
+      <nav className="home-nav">
+        <img src={logo} className="logo" alt="TrainHeroic Logo" />
+      </nav>
+    )
+  }
 }
