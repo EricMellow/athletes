7982ccbd7e8ebbc9da3eaa6c14cefe9e2b38f068
Eric Mellow <ericmellow@gmail.com>
2021-01-10 09:38:08 -0700

    Dynamically render table rows

--- code/src/components/workout/Workout.tsx
+++ code/src/components/workout/Workout.tsx
@@ -29,6 +29,22 @@ class Workout extends Component<WorkoutProps, WorkoutState> {
    let exercises = this.props.workout.blocks.map(async block => await getExercise(block.exerciseId))
    return Promise.all(exercises)
   }
+    if (this.state.exercises[0].id !== 0) {
+      let { workout } = this.props;
+      let tableRows = workout.blocks.map((block, i) => {
+        let sets = block.sets.map((set, ii) => {
+          return (
+            <tr key={block.exerciseId + ii}>
+              <td>{this.state.exercises[i]!.title}</td>
+              <td>{ii + 1}</td>
+              <td>{set.reps}</td>
+              <td>{set.weight || 0}</td>
+              <td>{set.reps * (set.weight || 0)}</td>
+            </tr>
+          )
+        })
+        return sets
+      })
   return (
     <div className="workout-container">
       I'm a workout
